830b4ff97581f114a87a326e377ee41d98cc3be6
Nikolaus Matten <nfmatten@gmail.com>
2022-09-07 13:08:08 -0600

    fix: main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import implementation
+
+def main():
+    implementation.months_pi_day()
+    implementation.fruits_and_veggies()
+    implementation.user_profile()
+    implementation.print_family()
+
+
+main()
